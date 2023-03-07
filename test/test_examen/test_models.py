32ba5dc529568c509a00033eff7ffc595fa73a3c
Nicolás Eduardo Rondón Ortiz <nrondon@ucompensar.edu.co>
2023-03-07 10:27:50 -0500

    add test for exam models

--- test/test_examen/test_models.py
+++ test/test_examen/test_models.py
@@ -1,6 +1,6 @@
 import pytest
 
-from apps.exams.models import Exam
+from apps.exams.models import Answer, Exam, Question
 
 
 @pytest.mark.django_db
@@ -9,3 +9,21 @@ def test_exam():
     exam.save()
     assert exam.name == "English Exam"
     assert exam.description == "This is a hard exam"
+
+
+@pytest.mark.django_db
+def test_question():
+    exam = Exam(name="English Exam", description="This is a hard exam")
+    exam.save()
+    question = Question.objects.create(text="Primera pregunta", exam_id=exam.id)
+    assert question.text == "Primera pregunta"
+    assert question.exam_id == exam.id
+
+
+@pytest.mark.django_db
+def test_answer():
+    exam = Exam(name="English Exam", description="This is a hard exam")
+    exam.save()
+    question = Question.objects.create(text="Primera pregunta", exam_id=exam.id)
+    answer = Answer.objects.create(text="1", question_id=question.id, is_correct=True)
+    assert answer.is_correct is True
